5b7064161897da90cd28b9da98e8c192fd97cd2b
chris.lee <neoxrock@gmail.com>
2021-11-29 10:05:28 +1100

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
+
+function App() {
+  return (
+    <div>
+      <CounterContainer></CounterContainer>
+      <SampleContainer></SampleContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/SampleContainer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Sample from '../components/Sample';
+import { getPost, getUsers } from '../modules/sample';
+
+const { useEffect } = React;
+const SampleContainer = ({ getPost, getUsers, post, users, loadingPost, loadingUsers }) => {
+  useEffect(() => {
+    // useEffect can not pass async, handle error inside
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fn();
+  }, [getPost, getUsers]);
+  return <Sample post={post} users={users} loadingPost={loadingPost} loadingUsers={loadingUsers} />;
+};
+
+export default connect(
+  ({ sample, loading }) => ({
+    post: sample.post,
+    users: sample.users,
+    loadingPost: loading['sample/GET_POST'],
+    loadingUsers: loading['sample/GET_POST'],
+  }),
+  {
+    getPost,
+    getUsers,
+  },
+)(SampleContainer);
